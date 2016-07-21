ebb13feec0cc8c391fcca2deb89488e30e9576bc
Glenn Ricaud <gri@enonic.com>
2016-07-21 13:59:35 +0200

    EXT-240 Simple ID Provider - Forgot password (Prototype)

--- src/main/resources/idprovider/idprovider.js
+++ src/main/resources/idprovider/idprovider.js
@@ -18,18 +18,28 @@ exports.handle401 = function (req) {
     };
 };
 
-exports.post = function (req) {
-    var body = JSON.parse(req.body);
+exports.login = function (req) {
+    var redirectUrl = req.validTicket ? req.params.redirect : generateRedirectUrl();
+    var body = generateLoginPage(redirectUrl);
+    return {
+        contentType: 'text/html',
+        body: body
+    };
+};
 
-    if (body.email) {
-        return handleForgotPassword(req, body.email);
-    } else if (body.token && body.password) {
-        return handleUpdatePwd(body.token, body.password);
+exports.logout = function (req) {
+    authLib.logout();
+
+    if (req.validTicket && req.params.redirect) {
+        return {
+            redirect: req.params.redirect
+        };
     }
 
+    var body = generateLoggedOutPage();
     return {
-        status: 400,
-        contentType: 'application/json'
+        contentType: 'text/html',
+        body: body
     };
 };
 
@@ -62,35 +72,24 @@ exports.get = function (req) {
     };
 };
 
-exports.login = function (req) {
-    var redirectUrl = req.validTicket ? req.params.redirect : generateRedirectUrl();
-    var body = generateLoginPage(redirectUrl);
-    return {
-        contentType: 'text/html',
-        body: body
-    };
-};
+exports.post = function (req) {
+    var body = JSON.parse(req.body);
 
-exports.logout = function (req) {
-    authLib.logout();
-
-    if (req.validTicket && req.params.redirect) {
-        return {
-            redirect: req.params.redirect
-        };
+    if (body.email) {
+        return handleForgotPassword(req, body.email);
+    } else if (body.token && body.password) {
+        return handleUpdatePwd(req, body.token, body.password);
     }
 
-    var body = generateLoggedOutPage();
     return {
-        contentType: 'text/html',
-        body: body
+        status: 400,
+        contentType: 'application/json'
     };
 };
 
 function isTokenValid(token) {
     var userInfo = infoByToken[token];
     return userInfo && (userInfo.timestamp - Date.now()) < 86400000
-
 }
 
 function handleForgotPassword(req, email) {
@@ -98,7 +97,10 @@ function handleForgotPassword(req, email) {
 
     var toEmailAddress;
     var body;
+
+    //If a user has the email provider
     if (user && user.email) {
+
         //Deletes existing token
         var existingToken = tokenByUser[user.email];
         if (existingToken) {
@@ -114,26 +116,24 @@ function handleForgotPassword(req, email) {
             timestamp: Date.now()
         };
 
-        //Sends the reset mail
+        //Prepares the reset email
         var passwordResetUrl = portalLib.idProviderUrl({params: {reset: token}, type: 'absolute'});
-        body = '<p>Somebody asked to reset your password on ' + req.host + '.<br/>' +
+        body = '<p>Somebody asked to reset your password on <a href="' + req.host + '">' + req.host + '</a>.<br/>' +
                'If it was not you, you can safely ignore this email.</p>' +
                '<p>To reset your password, click on the following link:</p>' +
                '<a href="' + passwordResetUrl + '">' + passwordResetUrl + '</a>';
     } else {
-        body = '<p>Somebody asked to reset your password on ' + req.host + '.<br/>' +
+
+        //Else, prepares a warning email
+        body = '<p>Somebody asked to reset your password on <a href="' + req.host + '">' + req.host + '</a>.<br/>' +
                'If it was not you, you can safely ignore this email.</p>' +
                '<p>There is no user linked to this email address.<br/>' +
                'You might have signed up with a different address</p>';
     }
 
-    mailLib.send({
-        from: 'noreply@gmail.com',
-        to: email,
-        subject: 'HTML email test',
-        body: body,
-        contentType: 'text/html; charset="UTF-8"'
-    });
+    //Sends email
+    sendMail(req, email, 'Password reset', body);
+
 
     return {
         body: {},
@@ -141,6 +141,47 @@ function handleForgotPassword(req, email) {
     };
 }
 
+function handleUpdatePwd(req, token, password) {
+    if (isTokenValid(token)) {
+        var userInfo = infoByToken[token];
+        runAdAdmin(function () {
+            authLib.changePassword({
+                userKey: userInfo.key,
+                password: password
+            });
+
+            authLib.login({
+                user: userInfo.email,
+                password: password,
+                userStore: portalLib.getUserStoreKey()
+            });
+        });
+
+        sendMail(req, userInfo.email, 'Password updated',
+            '<p>You have successfully changed your password on <a href="' + req.host + '">' + req.host + '</a>.</p>');
+
+        return {
+            body: {updated: true},
+            contentType: 'application/json'
+        };
+    }
+
+    return {
+        body: {updated: false},
+        contentType: 'application/json'
+    };
+}
+
+function sendMail(req, to, subject, body) {
+    var from = authLib.getIdProviderConfig().email || ('noreply@' + req.host);
+    mailLib.send({
+        from: from,
+        to: to,
+        subject: subject,
+        body: body,
+        contentType: 'text/html; charset="UTF-8"'
+    });
+}
 
 function findUserByEmail(email) {
     return runAdAdmin(function () {
@@ -167,34 +208,6 @@ function runAdAdmin(callback) {
             userStore: 'system'
         }
     }, callback);
-}
-
-function handleUpdatePwd(token, password) {
-    if (isTokenValid(token)) {
-        var userInfo = infoByToken[token];
-        runAdAdmin(function () {
-            authLib.changePassword({
-                userKey: userInfo.key,
-                password: password
-            });
-
-            authLib.login({
-                user: userInfo.email,
-                password: password,
-                userStore: portalLib.getUserStoreKey()
-            });
-        });
-
-        return {
-            body: {updated: true},
-            contentType: 'application/json'
-        };
-    }
-
-    return {
-        body: {updated: false},
-        contentType: 'application/json'
-    };
 }
 
 function generateLoginPage(redirectUrl) {
