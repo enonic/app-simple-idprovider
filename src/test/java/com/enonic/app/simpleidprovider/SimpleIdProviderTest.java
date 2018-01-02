7dda4be4ed867e51e2d6ed6110271735ed1a2feb
Glenn Ricaud <gri@enonic.com>
2018-01-02 14:17:21 +0100

    Simple ID Provider - Session timeout (#6)

--- src/test/java/com/enonic/app/simpleidprovider/SimpleIdProviderTest.java
+++ src/test/java/com/enonic/app/simpleidprovider/SimpleIdProviderTest.java
@@ -1,6 +1,6 @@
 package com.enonic.app.simpleidprovider;
 
-import com.enonic.xp.testing.script.ScriptRunnerSupport;
+import com.enonic.xp.testing.ScriptRunnerSupport;
 
 public class SimpleIdProviderTest
     extends ScriptRunnerSupport
