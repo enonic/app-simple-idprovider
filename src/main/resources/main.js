1ca75971830cda062de20548ce93af7c11504607
Persijn Kwekkeboom <pkw@enonic.com>
2021-07-23 19:49:04 +0200

    Copied app-user files for idprovider

--- src/main/resources/main.js
+++ src/main/resources/main.js
@@ -1,64 +1,30 @@
-const nodeLib = require('/lib/xp/node');
-const contextLib = require('/lib/xp/context');
+// const nodeLib = require('/lib/xp/node');
+const userAuth = require('/lib/auth');
+const contextLib = require('/lib/context');
 
 function exists(providers) {
-    for (const idprovider in providers) {
-        log.info(idprovider._name);
-        if (idprovider.idprovider) {
-            for (apps in [].concat(idprovider.idProvider)) {
-                if (apps.applicationKey === "com.enonic.app.simpleidprovider") {
-                    return true;
-                }
-            }
+    for (let i = 0; i < providers.length; i++) {
+        if (
+            providers[i].idProviderConfig &&
+            providers[i].idProviderConfig.applicationKey ===
+                'com.enonic.app.simpleidprovider'
+        ) {
+            return true;
         }
     }
     return false;
 }
 
 function initUserStore() {
-    const repoConnection = nodeLib.connect({
-        repoId: "system-repo",
-        branch: "master",
+    let systemidproviders;
+
+    contextLib.runAsAdmin(function () {
+        systemidproviders = userAuth.getIdProviders();
     });
 
-    log.info(`Exists: ${repoConnection.exists('/identity')}`);
-    const systemidproviders = repoConnection.findChildren({
-        start: 0,
-        count: 100,
-        parentKey: "identity",
-    });
-
-    log.info("connection established");
-    log.info(systemidproviders.total);
-
-    if (exists(systemidproviders.hits) === false) {
-        const result = contextLib.run(
-            {
-                repository: "system-repo",
-                branch: "master",
-                user: {
-                    login: "su",
-                    idProvider: "system",
-                },
-                principals: ["role:system.admin"],
-            },
-            createUserStore(repoConnection)
-        );
-
-        if (result) {
-            log.info("Created simple idprovider userstore");
-        }
+    if (exists(systemidproviders) === false) {
+        log.info('TODO setup new userstore');
     }
-}
-
-function createUserStore(connection) {
-    return connection.create({
-        _name: "simpleidprovider",
-        _parentPath: "/identity",
-        idProvider: {
-            applicationKey: "com.enonic.app.simpleidprovider",
-        },
-    });
 }
 
 initUserStore();
