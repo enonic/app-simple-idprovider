f644d8ddd68b45cfdfdca7420b95dd1f36bce034
Persijn Kwekkeboom <pkw@enonic.com>
2021-08-24 10:48:26 +0200

    Cluster safe user store init

--- src/main/resources/main.js
+++ src/main/resources/main.js
@@ -1,19 +1,20 @@
-const idproviderLib = require('/lib/idprovider');
-const context = require('/lib/context');
+const idproviderLib = require("/lib/idprovider");
+const context = require("/lib/context");
+const clusterLib = require("/lib/xp/cluster");
+const taskLib = require("/lib/xp/task");
 
 /**
  * Checks all idproviders if they are connected to the simpleidprovider
- * 
- * @param {Array} providers 
- * @returns {Boolean} true if exists, false if not 
+ *
+ * @param {Array} providers
+ * @returns {Boolean} true if exists, false if not
  */
-function exists(providers) {
+function exists(providers, key) {
     for (const count in providers) {
         if (
             providers[count] &&
             providers[count].idProviderConfig &&
-            providers[count].idProviderConfig.applicationKey ==
-                'com.enonic.app.simpleidprovider'
+            providers[count].idProviderConfig.applicationKey === key
         ) {
             return true;
         }
@@ -22,32 +23,30 @@ function exists(providers) {
 }
 
 function initUserStore() {
-    let systemidproviders;
-    context.runAsAdmin(function () {
-        systemidproviders = idproviderLib.getIdProviders();
-    });
+    const systemidproviders = idproviderLib.getIdProviders();
 
-    if (exists(systemidproviders) === false) {
-        let result;
-        context.runAsAdmin(function () {
-            result = idproviderLib.createIdProvider({
-                name: 'simpleid',
-                descripton: 'Default simple-idprovider',
-                idProviderConfig: {
-                    applicationKey: 'com.enonic.app.simpleidprovider',
-                    config: [
-                        //TODO set default config
-                    ],
-                },
-                permissions: [
-                ],
-            });
+    if (exists(systemidproviders, "com.enonic.app.simpleidprovider") === false) {
+        const result = idproviderLib.createIdProvider({
+            name: "simpleid",
+            descripton: "Default simple-idprovider",
+            idProviderConfig: {
+                applicationKey: "com.enonic.app.simpleidprovider",
+                config: [],
+            },
+            permissions: [],
         });
 
         if (result) {
-            log.info('Created simple idprovider userstore');
+            log.info("Created simple idprovider userstore");
         }
     }
 }
 
-initUserStore();
+if (clusterLib.isMaster()) {
+    context.runAsAdmin(function () {
+        taskLib.executeFunction({
+            description: "Create default simpleId userstore",
+            func: initUserStore,
+        });
+    });
+}
